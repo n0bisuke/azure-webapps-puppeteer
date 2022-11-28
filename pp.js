eeb9b8d2326f38573b69b69eb0e021d3fef7ad2d
n0bisuke <rsksound@gmail.com>
2022-11-28 18:21:16 +0900

    update pp

--- pp.js
+++ pp.js
@@ -1,12 +1,14 @@
 const puppeteer = require('puppeteer');
 
 const main = async () => {
+    console.log(`--start--`)
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://protoout.studio/');
-  await page.waitFor(10000);
+  await page.waitForTimeout(10000);
   await page.screenshot({path: 'example.png'});
   await browser.close();
+  console.log(`--done--`)
 };
 
 module.exports = main;
